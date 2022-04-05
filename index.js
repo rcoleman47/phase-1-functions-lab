d1507cc4c55b86c7b34a5d7020992b5df07afc78
ryanadams <rcoleman.adams@gmail.com>
2022-04-04 20:36:29 -0500

    commit tree e241f0bfcd560e7cd99b573e2393582c6e6fd18b

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block){
+    const hqBlock = 42
+    return Math.abs(block-hqBlock);
+};
+function distanceFromHqInFeet(block){
+   const dFT = (distanceFromHqInBlocks(block)*264);
+    return dFT;
+};
+function distanceTravelledInFeet(start, finish){
+    return Math.abs(start-finish)*264;
+};
+function calculatesFarePrice(start, destination){
+    let dist = Math.abs(start - destination)*264;
+    if (dist <= 400){
+        return 0;
+    } else if (dist <= 2000){
+        return ((dist - 400)*.02);
+    } else if (dist <= 2500){
+        return 25;
+    } else if (dist > 2500){
+        return 'cannot travel that far';
+    };
+};
